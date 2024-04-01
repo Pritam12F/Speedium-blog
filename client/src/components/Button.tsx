7cc6b0dba964e6f6dc5a6a0bf412a25768ee85d5
PritamDas <pritam.das.santu@gmail.com>
2024-04-01 17:17:20 +0530

    added some horizontal padding to the logout button

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -9,7 +9,7 @@ export const Button = (props: InputLabelType) => {
       className={
         props.label != "Logout"
           ? "bg-black text-white inline-block px-5 py-2 rounded-lg lg:px-32"
-          : "bg-black text-white inline-block text-sm px-1 mr-2 rounded-lg"
+          : "bg-black text-white inline-block text-sm px-2 mr-2 rounded-lg"
       }
       onClick={props.onClick}
     >
